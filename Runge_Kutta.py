9dae10bbe5e53477f89dd4ad578fb517b9189f4e
Ezhen <evgeny.ivanov@uliege.be>
2018-07-13 00:40:05 +0200

    3th

--- Runge_Kutta.py
+++ Runge_Kutta.py
@@ -1,83 +1,53 @@
-from pylab import *
-from netCDF4 import Dataset
 from numpy.linalg import inv
+import numpy as np
 
-# Subset the domain
-
-latmin = 70 #100
-latmax = 240 #200
-lonmin = 250 #75
-lonmax = 450 #150
-
-n = Dataset('/home/evgeny/Downloads/sv04-med-ingv-cur-an-fc-h_1531404445014.nc', 'r', format='NETCDF4')
-lon = n.variables['lon'][lonmin:lonmax]
-lat = n.variables['lat'][latmin:latmax]
-u = n.variables['uo'][0,0,latmin:latmax,lonmin:lonmax]
-v = n.variables['vo'][0,0,latmin:latmax,lonmin:lonmax]
-
-# put them on the cetner of the grid cells
-
-def pt():
-	#plotting function
-	lats,lons = np.meshgrid(lat,lon)
-	#quiver(lats[::3],lons[::3],u[::3,::3],v[::3,::3])
-	plot(x_free[1,:],x_free[0,:], label='no correction')
-	ylim(lat.min(),lat.max()); xlim(lon.min(),lon.max())
-	#scatter(x_pos[0,:],x_pos[1,:],s=155,c = P[0,0,:]+P[1,1,:], cmap='rainbow',edgecolors = None)
-	#colorbar()
-	#legend(loc=1)
-	show()
-
-
-def crd(lat_t,lon_t):
+def crd(lat_t,lon_t,lat,lon):
 	# calculation of coordinates in terms of a regular grid of a Copernicus product 
 	fract_j = (lon_t-lon[0])/(lon[1]-lon[0])
 	fract_i = (lat_t-lat[0])/(lat[1]-lat[0])
-	i,j = floor(fract_i),floor(fract_j)
+	i,j = np.floor(fract_i),np.floor(fract_j)
 	di,dj = fract_i-i,fract_j-j
 	#print(i,j,di,dj,fract_i,fract_j)
 	return i,j,di,dj
 
-def vel(t, x):
+def vel(t, x, lat,lon,u,v):
 	# check if a tracer still inside the domain
 	if x[1]>lon.max() or x[0]>lat.max() or x[1]<lon.min() or x[0]<lat.min():
 		return array([0,0])
-	i,j,di,dj = crd(x[0],x[1])
+	i,j,di,dj = crd(x[0],x[1],lat,lon)
 	j,i,dj,di = int(j),int(i),int(dj),int(di)
-	u00,u10,u01,u11 = u[i,j],u[i+1,j],u[i,j+1],u[i+1,j+1]
+	u00,u10,u01,u11 = tuple((np.array([u[i,j],u[i+1,j],u[i,j+1],u[i+1,j+1]])+np.array([u[i-1,j],u[i,j],u[i-1,j+1],u[i,j+1]])+np.array([u[i,j-1],u[i+1,j-1],u[i,j],u[i+1,j]])+np.array([u[i-1,j-1],u[i,j-1],u[i-1,j],u[i,j]]))/4)
 	ub = u00 + di * (u10-u00) 
-	ut = u10 + di * (u11-u10) 
+	ut = u10 + di * (u11-u01) 
 	ui = ub + dj * (ut - ub)
-	v00,v10,v01,v11 = v[i,j],v[i+1,j],v[i,j+1],v[i+1,j+1]
+	v00,v10,v01,v11 = tuple((np.array([v[i,j],v[i+1,j],v[i,j+1],v[i+1,j+1]])+np.array([v[i-1,j],v[i,j],v[i-1,j+1],v[i,j+1]])+np.array([v[i,j-1],v[i+1,j-1],v[i,j],v[i+1,j]])+np.array([v[i-1,j-1],v[i,j-1],v[i-1,j],v[i,j]]))/4)
 	vb = v00 + di * (v10-v00) 
-	vt = v10 + di * (v11-v10) 
+	vt = v10 + di * (v11-v01) 
 	vi = vb + dj * (vt - vb)
 	ui = ui / np.cos(np.deg2rad(x[0])) / 1.11e5
 	vi = vi / 1.11e5
-	#print ui,vi
-	return array([ui,vi])
+	return np.array([vi,ui])
 	
 # define runge-kutta function	
-def rgk(x, t, dt):
+def rgk(x, t, dt, lat,lon,u,v):
 	# Runge-Kutta discretization scheme
-	xp = x + dt * vel(t,x) / 2. 
-	x = x + dt * vel(t,xp)
+	xp = x + dt * vel(t,x,lat,lon,u,v) / 2. 
+	x = x + dt * vel(t,xp,lat,lon,u,v)
 	print(t,x)
 	return x
 
-# initialization
-dt = 100 	# s
-n_it = 2340 	# number of iterations
+def buoy(lat0,lon0,lat,lon,u,v,dt,n_it):
+	# declaration of array for a priori quantities
+	x_free = np.zeros((2,n_it)) # free
+	x_free[:,0] = [lat0,lon0]
 
-# declaration of array for a priori quantities
-x_free = np.zeros((2,n_it)) # free
-x_free[:,0] = [36.3,14.3]
-
-for i in range(1,n_it):
-	# calculation  of free x
-	x_free[:,i] = rgk(x_free[:,i-1], i, dt)
-
-pt()
+	for i in range(1,n_it):
+		# calculation  of free x
+		try:
+			x_free[:,i] = rgk(x_free[:,i-1], i, dt,lat,lon,u,v)
+		except:
+			return x_free[:,0:i-1]
+	return x_free
 	
 	
 
--- plot_probability_map.py
+++ plot_probability_map.py
@@ -9,6 +9,7 @@ import matplotlib as mpl
 import calendar
 from build_grid import *
 import cmocean.cm as cm
+from Runge_Kutta import buoy
 
 # Function plots velocity field nicely
 cmap = cm.speed
@@ -36,4 +37,16 @@ m1.contourf(x[:],y[:],w.T,clevs,cmap=cmap)
 prec = 3
 kk = m1.quiver(x[::prec,::prec],y[::prec,::prec],u.T[::prec,::prec],v.T[::prec,::prec],color='black',linewidth=w[::prec,::prec].flatten(),angles='xy',edgecolor='None') 
 mpl.colorbar.ColorbarBase(cax, cmap=cmap, boundaries = clevs, orientation='vertical')
+
+dt = 1800 	# s
+n_it = 1000 	# number of iterations
+lat0,lon0 = 36.7,13.2
+#lat0,lon0 = 37.1,15.8
+#lat0,lon0 = 38,11
+tracer = buoy(lat0,lon0,lat,lon,u,v,dt,n_it)
+xfree, yfree = m1(tracer[1], tracer[0])
+m1.plot(xfree[0],yfree[0], 'o',c='w',linewidth=50,label='initial position')
+m1.plot(xfree,yfree, label='no correction',linewidth=3)
+m1.plot(xfree[-1],yfree[-1], 'o',c='r',linewidth=50,label='last position')
+#plt.legend(loc=1)
 plt.show()
